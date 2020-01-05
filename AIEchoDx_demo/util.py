2b3784d0a3e9c5bdf582990d8912859af6818e37
Hao Chang <hao.chang@yale.edu>
2020-01-05 01:51:29 -0500

    ‘log’

--- AIEchoDx_demo/util.py
+++ AIEchoDx_demo/util.py
@@ -131,3 +131,30 @@ def remove_info4(video):
     video[mask == 0] = np.zeros((3, l))
     video = np.transpose(video, (3, 0, 1, 2))
     return (video)
+
+
+def convert_frames_to_video(pathIn, pathOut, fps):
+    frame_array = []
+    files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
+
+    # for sorting the file names properly
+    files.sort(key=lambda x: int(x[5:-4]))
+
+    for i in range(len(files)):
+        filename = os.path.join(pathIn, files[i])
+        # reading each files
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        # print(filename)
+        # inserting the frames into an image array
+        frame_array.append(img)
+
+    out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
+
+    return size
